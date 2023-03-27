f984660dc289517931713d1c7627c43e355eea0a
BlueAurum <yibragimov880@gmail.com>
2023-03-27 22:06:19 +0300

    add personal accaunt page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import { MainPage } from './MainPage/MainPage';
 import { HomePage } from './HomePage/HomePage'
 import { ExamplePage } from './ExamplePage/ExamplePage'
+import { PersonalAccountPage } from './PersonalAccountPage/PersonalAccountPage';
 
-export { ExamplePage, HomePage, MainPage }
+export { ExamplePage, HomePage, MainPage, PersonalAccountPage }
