3ca6cebf9d3d1da30b527173c69c17061cf8ad7a
amirkhan_web <amirhanvalman@gmail.com>
2023-05-02 21:30:27 +0300

    init

--- src/features/Intro/Intro.tsx
+++ src/features/Intro/Intro.tsx
@@ -1,28 +1,32 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import { Logo } from 'components'
+import { Button, Logo } from 'components'
 
 import { SiteData } from 'shared/types/site'
 import IconDown from '/public/assets/icons/iconDown.png'
 
+import { LoginRegistrationModal } from 'features/LoginRegistrationModal/LoginRegistrationModal'
+
 import s from './intro.module.scss'
 
 export const Intro: FC<SiteData> = ({ logo, desc, title }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className={s.intro}>
       <div className={s.backgroundMask} />
-
       <div className={s.container}>
         {logo && title ? (
-          <Logo logoImage={logo} logoText={title} classNames={s.logo} />
+          <>
+            <Logo logoImage={logo} logoText={title} classNames={s.logo} />
+          </>
         ) : (
           <div className={s.logoGroup}>
             <div className={s.logo}>
               <Image layout='fill' alt='logo' src='/assets/icons/Logo.png' />
             </div>
-
             <div className={s.logoText}>
               <Image
                 src='/assets/icons/Bors.svg'
@@ -32,7 +36,15 @@ export const Intro: FC<SiteData> = ({ logo, desc, title }) => {
             </div>
           </div>
         )}
-
+        <Button className={s.introBtn}>
+          Registrera dig
+        </Button>
+        <LoginRegistrationModal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(true)}
+          setIsOpenPasswordRecovery={() => setIsOpen(true)}
+          openPopup={() => setIsOpen(true)}
+        />
         <p className={s.text}>{desc ? desc : 'Error text not found'}</p>
 
         <Link href={'#info'} scroll={false}>
